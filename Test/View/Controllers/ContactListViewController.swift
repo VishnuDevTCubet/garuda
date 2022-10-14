8393a7ea7ec2ad018bfb22828673e65db7e13c7e
Annamalai, Sivakumar <Sivakumar.Annamalai@sc.com>
2022-10-14 16:11:49 +0530

    no name

--- Test/View/Controllers/ContactListViewController.swift
+++ Test/View/Controllers/ContactListViewController.swift
@@ -11,6 +11,9 @@ class ContactListViewController: UIViewController {
     var contactListViewModel = ContactListViewModel()
     @IBOutlet weak var tableView: UITableView!
 
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
